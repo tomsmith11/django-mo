500d9a15f80fb18198f6dae68693b509357117fa
Tom Smith <tom@craftscale.uk>
2024-10-22 13:02:53 +0100

    README.md update

--- myapp/views.py
+++ myapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Welcome to the home page!")
+    return HttpResponse("This site will be used to showcase my django development, and will likely not work properly or function like a normal website for a while.")

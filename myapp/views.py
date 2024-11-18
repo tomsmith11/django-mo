8cf24bf8825f94eb4230501f395ad687d5ee7527
Tom Smith <tom@craftscale.uk>
2024-11-18 22:13:19 +0000

    Added functioning submission of form

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import User
 
 def home(request):
+    if request.method == 'POST':
+        # Get form data
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        # Create user
+        user = User(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            password=password
+        )
+        user.save()
+
+        return HttpResponse('User created successfully')
     return render(request, 'home.html')

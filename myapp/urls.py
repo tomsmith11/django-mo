b019232face465ac3a184445e22d2c0f97fcee89
Tom Smith <tom@craftscale.uk>
2024-11-27 22:57:24 +0000

    Created new home login and register pages

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,8 +3,11 @@ from django.urls import path, include  # Ensure include is imported
 from myapp import views
 
 from django.urls import path
-from . import views  # Import views from the current directory
+from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Home view from myapp
+    path('', views.home, name='home'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
